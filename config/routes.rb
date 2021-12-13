2905d8dbb290e65454df87f8a7b89a3b3d76caa2
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-12-13 14:27:34 -0600

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :bmis
-  resources :exercises
-  resources :users
+  
+  resources :users do
+    resources :exercises
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
