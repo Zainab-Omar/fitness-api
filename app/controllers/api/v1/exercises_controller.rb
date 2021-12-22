c897b436b67af4cc3961770211d08ee1acf41eb3
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-12-22 15:41:29 -0600

    build index, show, create, destory method for  exercises controller

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::ExercisesController < ApplicationController
+    skip_before_action :authorized
+
+    def index
+        exercises = Exercise.all
+        render json: @exercises
+    end
+
+    def create
+        exercise = Exercise.create(exercise_params)
+        if exercise.save
+            render json: exercise
+        else
+            render json: {errors: exercise.errors.full_messages}
+        end
+    end
+
+    def show
+        exercise = Exercise.find(params[:id])
+        render json: exercise
+    end
+
+    def destroy
+        exercise = Exercise.find(params[id])
+        exercise.destroy
+        render json: exercise
+    end
+
+    private
+    def exercise_params
+        params.require(:exercise).permit(:body_part, :equipment, :gif_url, :name, :target, :user_id)
+    end
+    
 end
+
