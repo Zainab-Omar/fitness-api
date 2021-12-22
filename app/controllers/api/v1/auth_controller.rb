b078135c9eccccfce6da96955307682904b3183f
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-12-22 15:25:37 -0600

    build user_params method

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -11,5 +11,10 @@ class Api::V1::AuthController < ApplicationController
             render json: { message: 'Invalid username or password' }, status: :unauthorized
         end
     end
-    
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end
