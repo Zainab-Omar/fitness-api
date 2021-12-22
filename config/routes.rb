a510c435f409ed508feed0c9121098c37494555d
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-12-22 15:55:26 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,31 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  namespace :api do
-    namespace :v1 do
-      resources :bmis
-    end
-  end
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :bmis
+  #   end
+  # end
   
   namespace :api do
     namespace :v1 do
       resources :users do
-        get '/profile', to: 'users#profile'
         resources :exercises
       end
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :bmis
+      end
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get '/profile', to: 'users#profile'
     end
   end
 
