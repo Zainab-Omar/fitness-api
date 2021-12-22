c8bb1435e2372b123a57991b82ecb9fcbd65fec1
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-12-22 15:12:41 -0600

    build create method in users controller to create new user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,15 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    # signing up a user
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = encode_token(user_id:@user.id)
+            render json: {user: UserSerializer.new(@user), jwt: @token}, status: :created
+        else
+            render json: { message: @user.errors.full_messages }, status: :not_acceptable
+        end
+    end
 
 end
