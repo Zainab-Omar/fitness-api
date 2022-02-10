f28e0df85e0e07373689e3570f54d0d9cfd3b464
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-02-10 17:28:13 -0600

    build create method for BMI controller

--- app/controllers/api/v1/bmis_controller.rb
+++ app/controllers/api/v1/bmis_controller.rb
@@ -2,10 +2,17 @@ class Api::V1::BmisController < ApplicationController
     skip_before_action :authorized
     before_action: :set_user
 
-    
+
     def create
         binding.pry
+        bmi = @user.bmis.new(bmi_params)
+        if bmi.save
+            render json: bmi
+        else
+            render json: {errors: bmi.errors.full_messages}
+        end
     end
+
 
     private
     def bmi_params
