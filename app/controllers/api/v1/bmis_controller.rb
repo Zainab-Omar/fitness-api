39de6339b97efd478504731b6dec9f3bff492e0c
Zainab Omar <zainab.alshaikhli01@gmail.com>
2022-02-10 17:25:32 -0600

    create set_user and bmi_params methods

--- app/controllers/api/v1/bmis_controller.rb
+++ app/controllers/api/v1/bmis_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::BmisController < ApplicationController
     skip_before_action :authorized
+    before_action: :set_user
+
+    
     def create
         binding.pry
+    end
+
+    private
+    def bmi_params
+        params.require(:bmi).permit(:bmi, :health, :healthy_bmi_range, user_id)
+    end
+
+    def set_user
+        @user = User.find(params[:user_id])
     end
     
 end
